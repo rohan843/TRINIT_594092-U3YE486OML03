3125863a3fd79cb8627f6aacfdb4a51d2d0b5a55
rohan843 <rohan.sharma2934@gmail.com>
2023-02-11 20:09:37 +0530

    add ml algos

--- mlAlgos.py
+++ mlAlgos.py
@@ -2,95 +2,50 @@ import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeRegressor
 
+crop_recomendation_df = pd.read_csv('/kaggle/input/tri-nit/data/crop_recommendation.csv')
+rainfall_levels_df = pd.read_csv('/kaggle/input/tri-nit/data/rainfall_levels.csv')
+agriculture_price_df = pd.read_csv('/kaggle/input/tri-nit/data/agriculture_price.csv')
 
-files = ["/kaggle/input/crop-recommendation-dataset/Crop_recommendation.csv","/kaggle/input/rainfall-in-india/rainfall in india 1901-2015.csv","/kaggle/input/rainfall-in-india/district wise rainfall normal.csv"]
-crop_recomendation_df=pd.read_csv(files[0])
-district_wise_rainfall_df = pd.read_csv(files[2])
-rainfall_in_india_df=pd.read_csv(files[1])
+# N, P, K, pH
 
-# crop_recomendation_df.head()
+tmp_crop_recs_df = pd.get_dummies(crop_recomendation_df)[['temperature', 'humidity', 'label_apple',
+       'label_banana', 'label_coconut', 'label_cotton', 'label_grapes',
+       'label_jute', 'label_maize', 'label_mango', 'label_orange',
+       'label_papaya', 'label_pomegranate', 'label_rice', 'N', 'P', 'K', 'ph']]
 
-# crop_recomendation_df.info()
+models = {
+    'N': DecisionTreeRegressor().fit(tmp_crop_recs_df.iloc[:, :-4].values, tmp_crop_recs_df.iloc[:, -4].values),
+    'P': DecisionTreeRegressor().fit(tmp_crop_recs_df.iloc[:, :-4].values, tmp_crop_recs_df.iloc[:, -3].values),
+    'K': DecisionTreeRegressor().fit(tmp_crop_recs_df.iloc[:, :-4].values, tmp_crop_recs_df.iloc[:, -2].values),
+    'pH': DecisionTreeRegressor().fit(tmp_crop_recs_df.iloc[:, :-4].values, tmp_crop_recs_df.iloc[:, -1].values)
+}
 
-# crop_recomendation_df.describe()
-
-# crop_recomendation_df.label.describe()
-
-temp_df=pd.read_csv("/kaggle/input/crop-recommendation-dataset/Crop_recommendation.csv")
-
-temp_df=pd.get_dummies(temp_df)
-#temp_df.head()
-
-temp_df = temp_df[['temperature', 'humidity', 'rainfall', 'label_apple', 'label_banana', 'label_blackgram', 'label_chickpea', 'label_coconut', 'label_coffee', 'label_cotton', 'label_grapes', 'label_jute', 'label_kidneybeans', 'label_lentil', 'label_maize', 'label_mango', 'label_mothbeans', 'label_mungbean', 'label_muskmelon', 'label_orange', 'label_papaya', 'label_pigeonpeas', 'label_pomegranate', 'label_rice', 'label_watermelon','N', 'P', 'K','ph']]
-
-#temp_df.head()
-
-# Models
-#ToDO
-N_model = DecisionTreeRegressor()
-N_model.fit(temp_df.iloc[:,:-4].values,temp_df.iloc[:,-4].values)
-
-#N_model.predict([[20.130175,81.604873262,2.717340,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0]])
-
-P_model = DecisionTreeRegressor()
-P_model.fit(temp_df.iloc[:,:-4].values,temp_df.iloc[:,-3].values)
-# P_model.predict([[20.130175,81.604873,2.717340,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0]])
-
-K_model = DecisionTreeRegressor()
-K_model.fit(temp_df.iloc[:,:-4].values,temp_df.iloc[:,-2].values)
-
-#K_model.predict([[20.130175,81.604873,2.717340,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0]])
-
-
-Ph_model = DecisionTreeRegressor()
-Ph_model.fit(temp_df.iloc[:,:-4].values,temp_df.iloc[:,-1].values)
-# Ph_model.predict([[20.130175,81.604873,2.717340,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0]])
-
-# sns.histplot(data=crop_recomendation_df, x="label")
-# rainfall_in_india_df.head()
-# rainfall_in_india_df.info()
-rainfall_in_india_df=rainfall_in_india_df.dropna()
-# rainfall_in_india_df.info()
-
-def getValues(subdivision,month):
-    df_new = rainfall_in_india_df[['SUBDIVISION',month,'YEAR']]
-    df_new = df_new[df_new['SUBDIVISION']==subdivision]
-    df_new = df_new.drop(['SUBDIVISION'],axis=1)
-    final_list = df_new.values.tolist()
-    return final_list
-
-# getValues('ANDAMAN & NICOBAR ISLANDS','JAN') 
-# district_wise_rainfall_df.head()  
-# district_wise_rainfall_df.info()
-# district_wise_rainfall_df.describe()
-
-all_agriculture_price_df=pd.read_csv("/kaggle/input/all-agriculture-related-datasets-for-india/csv")
-# all_agriculture_price_df.head()
-# all_agriculture_price_df.info()
-# all_agriculture_price_df.commodity.unique()
-# s1=set([i.lower() for i in crop_recomendation_df.label.unique()])
-# s2=set([i.lower() for i in all_agriculture_price_df.commodity.unique()])
-# print(s1 & s2)
-
-temp_df1=all_agriculture_price_df[['state','commodity','modal_price']]
-# temp_df1.head()
-
-def getDetails(state):
-    df_new = temp_df1[temp_df1['state']==state]
-    df_new = df_new.drop(['state'],axis=1)
-    final_list = df_new.values.tolist()
-    return final_list
-
-#getDetails('Andaman and Nicobar')   
-
-#Soil Details to crop
-knn_model = KNeighborsClassifier()
-knn_model.fit(crop_recomendation_df.iloc[:,:-1].values,crop_recomendation_df.iloc[:,-1].values)
-
-#knn_model.predict([[95,42,43,20.879744,100.002744,6.502985,50.935536]])
-
-
-
+def toBinary(prop):
+    return 1 if prop else 0
     
-    
-    
+def getOneHotEncodedVector(crop):
+    return [toBinary('apple' == crop),
+       toBinary('banana' == crop), toBinary('coconut' == crop), toBinary('cotton' == crop), toBinary('grapes' == crop),
+       toBinary('jute' == crop), toBinary('maize' == crop), toBinary('mango' == crop), toBinary('orange' == crop),
+       toBinary('papaya' == crop), toBinary('pomegranate' == crop), toBinary('rice' == crop)]
+
+# [temperature, humidity] + getOneHotEncodedVector(crop)
+# models['N'].predict([tmp_crop_recs_df.iloc[0, :-4].values])[0]
+
+# Rainfall Levels
+
+# months = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
+def getRainfallLevelValues(state, month):
+    return [tuple(i) for i in rainfall_levels_df[rainfall_levels_df['state'] == state][[month, 'YEAR']].values]
+
+# State based Price of Items
+
+def getStateBasedItemPrices(state):
+    return [tuple(i) for i in agriculture_price_df[agriculture_price_df['state'] == state][['label', 'modal_price']].values]
+
+# Crop Recommendation based on Soil
+
+crop_recommender_model = KNeighborsClassifier().fit(crop_recomendation_df.iloc[:, :-1].values, crop_recomendation_df.iloc[:, -1].values)
+
+# [N, P, K, temperature, humidity, ph]
+# crop_recommender_model.predict([crop_recomendation_df.iloc[0, :-1].values])[0]
