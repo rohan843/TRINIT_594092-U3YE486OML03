53cbd25266655cebdd3444953d46acb859120cc3
rohan843 <rohan.sharma2934@gmail.com>
2023-02-11 15:35:52 +0530

    add js code

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const parser = require('body-parser');
+const axios = require('axios');
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -8,6 +9,56 @@ app.use("*/css", express.static("public/css"));
 app.use("*/img", express.static("public/img"));
 app.use("*/js", express.static("public/js"));
 
+// ---- Utility Functions ----
+const baseURL = '';
+
+async function apiCall(url, params) {
+    let res = await axios.get(url, {
+        params: params
+    });
+    return res.data;
+}
+
+async function get_crop_suggestion(N, P, K, pH, state) {
+    let res = await apiCall(baseURL + '/crop_suggestion', {
+        N: N,
+        P: P,
+        K: K,
+        pH: pH,
+        state: state
+    });
+    return res['best_crop'];
+}
+
+async function get_soil_suggestion(crop, state) {
+    let res = await apiCall(baseURL + '/soil_suggestion', {
+        crop: crop,
+        state: state
+    });
+    return {
+        'N': res['N'],
+        'P': res['P'],
+        'K': res['K'],
+        'pH': res['pH']
+    };
+}
+
+async function get_rainfall_timeseries_data(month, state) {
+    let res = await apiCall(baseURL + '/rainfall_timeseries_data', {
+        month: month,
+        state: state
+    });
+    return res['timeseries_data'];
+}
+
+async function crop_price_by_state_data(state) {
+    let res = await apiCall(baseURL + '/crop_price_by_state_data', {
+        state: state
+    });
+    return res['price_data'];
+}
+
+// ---- Endpoints ----
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
